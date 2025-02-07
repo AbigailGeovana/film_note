acd7c5c1539ecc5270907ef581ff2423b0767c0b
AbigailPedreira <abigailpedreira6@gmail.com>
2025-02-07 14:01:19 -0300

    Primeiro commit do meu app Film Note

---
+++ android/app/src/main/kotlin/com/example/film_note/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.film_note
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/presentation/pages/home_page/widgets/topbar_widget.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+List<String> titles = <String>[
+  'Profile',
+  ''
+];
